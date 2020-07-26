629a5cba8d13b68b6bb81ceab7b28b5d43ce5e2a
Williams David A <WilliamsDavidA@JohnDeere.com>
2020-07-26 12:09:53 -0500

    created game functions

--- memory-game-download/memory-game (1)/js/main.js
+++ memory-game-download/memory-game (1)/js/main.js
@@ -1,22 +1,47 @@
 //console.log("Up and running!");
 
-let cards = ["queen","queen","king","king"];
+let cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/qeen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+
+];
 let cardsInPlay = [];
 
 function checkForMatch () {
 	
 	if(cardsInPlay[0] === cardsInPlay[1]) {
-		console.log("You found a match!");
+		alert("You found a match!");
 	}
 	else {
-		console.log("Sorry, try again.");
+		alert("Sorry, try again.");
 	}
 }
 
 function flipCard (cardId) {
 
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].suit);
+	console.log(cards[cardId].cardImage);
+
+	cardsInPlay.push(cards[cardId].rank);
 
 	if (cardsInPlay.length === 2){
 		checkForMatch ();
@@ -32,9 +57,8 @@ flipCard(2);
 
 
 
-
-
 // old code
+//let cards = ["queen","queen","king","king"];
 //	let cardOne = cards[0];
 //	let cardTwo = cards[2];
 //	cardsInPlay.push(cardOne);
